538b6e7d3705baed68de86c2cce6fe1628b67b0e
naseeb88900 <naseebahmed7263@gmail.com>
2025-08-27 23:25:51 +0500

    added payment funtionality

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { handleWebhook } from "../controllers/payment";
+import { authenticateToken } from "../middlewares/auth";
 
 const router = Router();
-
+router.use(authenticateToken);
 router.post("/stripe", handleWebhook);
 
 export default router;
